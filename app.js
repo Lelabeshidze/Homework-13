60c95fd08f5102d4eac57371fa27dda1dbe6a005
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2021-12-22 14:12:33 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
 const myName = 'Lela';
 const myAge = 22;
-const amIAdult = true;
 const myHobby= 'Photography'; 
 console.log ('My name is ' + myName + '. I am '+ myAge + ' old and my hobby is ' +myHobby);
