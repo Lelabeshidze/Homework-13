e5989396a33d3b9166604489b5095ae57a0d5a03
Lelabeshidze <lela.beshidze@gmail.com>
2021-12-23 17:58:45 +0400

    added types

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-
+var myname2 = 'Lela2';
+let myname1 ='Lela1';
+const myString = 'String';
+const myNumber = 559454;
+const myBoolean = false;
+const myBoolean1 = true;
+const myNull =null;
+const myUndefined='Undefined'
 const myName = 'Lela';
 const myAge = 22;
 const amIAdult = true;
 const myHobby= 'Photography'; 
 console.log ('My name is ' + myName + '. I am '+ myAge + ' old and my hobby is ' +myHobby);
+console.log(myString);
+console.log (myNumber);
+console.log(myBoolean);
+console.log(myBoolean1);
+console.log (myNull);
+console.log (myUndefined);
